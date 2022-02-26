8863bcf1803a2ecbfed09665be061f57b0871012
ibt-ihal <cheribtihal28@gmail.com>
2022-02-27 00:10:37 +0100

    tp3

---
+++ src/ma/education/tp2/statickeyword/Etudiant.java
@@ -0,0 +1,19 @@
+package ma.education.tp2.statickeyword;
+
+public class Etudiant {
+    public Etudiant(long id, String nom, int nbEtud) {
+        this.id = id;
+        this.nom = nom;
+        nbEtud++;
+    }
+
+    public long id;
+    public String nom;
+    public int nbEtud;
+
+    public static void main(String[] args) {
+        Etudiant e1 = new Etudiant(1, "FATIMA", 22);
+        Etudiant e2 = new Etudiant(2, "HIKMAT", 10);
+        System.out.println("le id :" + e1.id + "le nom: " + e1.nom + " num etudiant :" + e1.nbEtud);
+    }
+}
